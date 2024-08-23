15d6ff914b6b445120b351d5f93908c31c5229a0
dhonevrushabh <dhonevrushabh07@gmail.com>
2024-08-24 00:46:22 +0530

    1st episode

---
+++ App.js
@@ -0,0 +1,50 @@
+// const heading = React.createElement("h1", { id: "heading" }, "Hello Wolrd from React using App.js file")
+
+/**
+ * <div id="parent">
+ *      <div id="children">
+ *              <h1>I'm a h1 tag</h1>
+ *      </div>
+ * </div>
+ *
+ * const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I'm h1 tag")))
+ */
+
+/**
+ * <div id="parent">
+ *      <div id="children">
+ *              <h1>I'm a h1 tag</h1>
+ *              <h2>I'm a h2 tag</h2>
+ *      </div>
+ * </div>
+ *
+ * const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]))
+ */
+
+/**
+ * <div id="parent">
+ *      <div id="children1">
+ *              <h1>I'm a h1 tag</h1>
+ *              <h2>I'm a h2 tag</h2>
+ *      </div>
+ *      <div id="children2">
+ *              <h1>I'm a h1 tag</h1>
+ *              <h2>I'm a h2 tag</h2>
+ *      </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
