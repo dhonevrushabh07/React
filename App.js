67ae73cb004a65dae545ed7428ecec0f76531cf5
dhonevrushabh <dhonevrushabh07@gmail.com>
2024-08-25 20:46:06 +0530

    episode-4

--- App.js
+++ App.js
@@ -37,16 +37,31 @@ import ReactDOM from "react-dom/client";
  *
  */
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "I'm h1 tag"),
+//     React.createElement("h2", {}, "I'm h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm h1 tag"),
+//     React.createElement("h2", {}, "I'm h2 tag"),
+//   ]),
+// ]);
+
+//This is React Element
+const heading = (
+  <h1 id="title" key="root">Namaste React</h1>
+)
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* How to use React element in component */}
+      {heading}
+      <h2>Namaste React functional component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeaderComponent />);
